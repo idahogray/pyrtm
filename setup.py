cc17c3abb54259bad0b6eeda5ccefe7aea891430
srid <srid@localhost>
2009-12-08 13:18:51 -0800

    post-release (0.2) version-up'ing to 0.3dev

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='pyrtm',
-      version='0.2dev',
+      version='0.3dev',
       description='Remember The Milk API',
       author='Sridhar Ratnakumar',
       author_email='srid@nearfar.org',
